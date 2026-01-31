521d4f20b8e23e1d56e0181d5b3592cbe459c245
JosephGarcia98 <garciajoseph998@yahoo.com>
2026-01-31 00:23:43 -0500

    Initialize game state on DOMContentLoaded
    Set initial text for current turn and button label on DOM load.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ import { curTurn, mBtn, infiniteBtn } from './dom.js';
 import { highlightOldest } from './gamelogic.js';
 import { gameState } from './gamestate.js';
 
+//run after DOM is loaded
 document.addEventListener("DOMContentLoaded", () => {
+    //set text
     curTurn.textContent = "Player X's turn";
     infiniteBtn.textContent = "Infinite\nMode";
+    //sets up events
     allEvents();
     highlightOldest(gameState.curPlayer);
 });
