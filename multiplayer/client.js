4492550c7f596813dacdc87b549f4a9a0f63ac64
JosephGarcia98 <garciajoseph998@yahoo.com>
2026-02-01 16:59:33 -0500

    Add files via upload

---
+++ multiplayer/client.js
@@ -0,0 +1,11 @@
+//everyhthing is commented out so came can run local
+// Multiplayer client (WIP – not enabled yet)
+// const socket = io();
+// socket.on("move", (data) => {
+//   console.log("Received move:", data);
+//   updateBoard(data.cellIndex);
+// });
+
+// function sendMove(cellIndex) {
+//   socket.emit("move", { cellIndex });
+// }
