85472a6d2344dd6437e3af02fa1d24f38f3c0544
JosephGarcia98 <garciajoseph998@yahoo.com>
2026-02-02 07:06:34 -0500

    Update client.js

--- multiplayer/client.js
+++ multiplayer/client.js
@@ -1,5 +1,4 @@
 //everyhthing is commented out so came can run local
-// Multiplayer client (WIP – not enabled yet)
 // const socket = io();
 // socket.on("move", (data) => {
 //   console.log("Received move:", data);
