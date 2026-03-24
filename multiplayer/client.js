69f4fd7ba6f9a6256efd32c27f981152366e4bfb
JosephGarcia98 <garciajoseph998@yahoo.com>
2026-03-24 01:48:25 -0400

    Delete multiplayer directory

--- multiplayer/client.js
+++
@@ -1,9 +0,0 @@
-// const socket = io();
-// socket.on("move", (data) => {
-//   console.log("Received move:", data);
-//   updateBoard(data.cellIndex);
-// });
-
-// function sendMove(cellIndex) {
-//   socket.emit("move", { cellIndex });
-// }
